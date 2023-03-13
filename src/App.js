4b19a0c24703da24ed0c147491b34cbf2c453b99
Luiz Santos <luizfelipe0598@icloud.com>
2023-03-13 16:25:12 +0000

    pushing all the files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Josh from "./components/josh/josh";
+import Luiz from "./components/luiz/luiz";
+import Juliana from "./components/juliana/juliana";
+import Rachel from "./components/rachel/rachel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Josh />
+      <Luiz />
+      <Juliana />
+      <Rachel />
+  </div>
   );
 }
 
